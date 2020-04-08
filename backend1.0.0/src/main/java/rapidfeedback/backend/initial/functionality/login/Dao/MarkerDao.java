f1495672d71e6bca2aa2fb5e0c95fe28a300a88a
yutingcai-cyt <531546130@qq.com>
2020-04-08 23:16:44 +0800

    add a dao

---
+++ backend1.0.0/src/main/java/rapidfeedback/backend/initial/functionality/login/Dao/MarkerDao.java
@@ -0,0 +1,12 @@
+package rapidfeedback.backend.initial.functionality.login.Dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface MarkerDao {
+
+
+}
