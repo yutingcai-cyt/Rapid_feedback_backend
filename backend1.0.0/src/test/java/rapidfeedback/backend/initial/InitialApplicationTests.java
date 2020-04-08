522a37a1670ad318a79291cb71e08fc46e54c71f
yutingcai-cyt <531546130@qq.com>
2020-04-08 22:31:30 +0800

    change to yml

--- backend1.0.0/src/test/java/rapidfeedback/backend/initial/InitialApplicationTests.java
+++ backend1.0.0/src/test/java/rapidfeedback/backend/initial/InitialApplicationTests.java
@@ -1,13 +1,27 @@
 package rapidfeedback.backend.initial;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import rapidfeedback.backend.initial.mapper.MarkerMapper;
+import rapidfeedback.backend.initial.model.Marker;
+
+import java.util.List;
 
 @SpringBootTest
 class InitialApplicationTests {
 
+
+	@Autowired
+	private MarkerMapper markerMapper;
+
 	@Test
-	void contextLoads() {
+	void findAllTest(){
+		List<Marker> list = markerMapper.findAll();
+		for(Marker m : list){
+			System.out.println(m);
+		}
 	}
 
 }
