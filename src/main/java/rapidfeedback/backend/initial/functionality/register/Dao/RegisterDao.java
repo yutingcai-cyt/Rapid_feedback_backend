6b340111cc5844b32d66a12d3efd79cb22358d60
yutingcai-cyt <531546130@qq.com>
2020-04-27 16:39:12 +0800

    add regiter function

---
+++ src/main/java/rapidfeedback/backend/initial/functionality/register/Dao/RegisterDao.java
@@ -0,0 +1,8 @@
+package rapidfeedback.backend.initial.functionality.register.Dao;
+
+/**
+ * @author cyt
+ * @date 2020/4/26
+ */
+public interface RegisterDao {
+}
---
+++ src/main/java/rapidfeedback/backend/initial/functionality/register/Service/RegisterService.java
@@ -0,0 +1,8 @@
+package rapidfeedback.backend.initial.functionality.register.Service;
+
+/**
+ * @author cyt
+ * @date 2020/4/26
+ */
+public interface RegisterService {
+}
