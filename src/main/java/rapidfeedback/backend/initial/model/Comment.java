9b862f9ff0bfb94efa5f0d9ccc08042baf9d7422
yutingcai-cyt <531546130@qq.com>
2020-04-29 19:20:46 +0800

    test

--- src/main/java/rapidfeedback/backend/initial/model/Comment.java
+++ src/main/java/rapidfeedback/backend/initial/model/Comment.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Builder
 @Data
 public class Comment {
-    private Integer id;
+    private Integer id11;
     private String text;
     private Integer idField;
     private String type;
