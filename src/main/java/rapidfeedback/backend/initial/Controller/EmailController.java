50841e216e66bc6fbbb1dc20ce011a3d1543ddae
yutingcai-cyt <531546130@qq.com>
2020-06-01 23:10:54 +0800

    change excel

--- src/main/java/rapidfeedback/backend/initial/Controller/EmailController.java
+++ src/main/java/rapidfeedback/backend/initial/Controller/EmailController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import rapidfeedback.backend.initial.CommonTools.Token.Token;
 import rapidfeedback.backend.initial.functionality.email.Service.EmailService;
 import rapidfeedback.backend.initial.functionality.email.model.EmailRequest;
 
@@ -27,8 +28,8 @@ public class EmailController {
 
     @PostMapping("/email")
     public CompletableFuture<ResponseEntity<Void>> sendEmail(HttpServletRequest request, @RequestBody EmailRequest emailRequest){
-//        String token = request.getHeader("Authorization");
-//        Token.tokenCheck(request,token);
+        String token = request.getHeader("Authorization");
+        Token.tokenCheck(request,token);
         return emailService.sendEmail(emailRequest.getProject_id(),emailRequest.getStudentIdList(),emailRequest.getOption())
                 .thenApplyAsync(aVoid -> {
                     log.info("send email for project "+emailRequest.getProject_id());
@@ -38,8 +39,8 @@ public class EmailController {
 
     @PostMapping("/audio")
     public CompletableFuture<ResponseEntity<Void>> uploadAudio(HttpServletRequest request, @RequestParam("audio")MultipartFile file, @RequestParam("project_id") Integer projectId, @RequestParam("studentIdList")List<Integer> studentIdList){
-//        String token = request.getHeader("Authorization");
-//        Token.tokenCheck(request,token);
+        String token = request.getHeader("Authorization");
+        Token.tokenCheck(request,token);
         return emailService.addAudio(projectId,studentIdList,file).thenApplyAsync(aVoid ->{
            log.info("save audio for porject {} and student {}",projectId,studentIdList);
            return ResponseEntity.ok(aVoid);
